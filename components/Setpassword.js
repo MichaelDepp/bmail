50cedbd74b59981cc93a03818f96c7c697233984
MichaelDepp <michaeldepp7@gmail.com>
2020-11-20 00:42:42 +0800

    signin & signup page setup

---
+++ components/Setpassword.js
@@ -0,0 +1,126 @@
+import {
+  Box,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  Input,
+  Flex,
+  Center,
+  Button,
+} from "@chakra-ui/react";
+import Signlayout from "../components/Signlayout";
+import { AiFillThunderbolt } from "react-icons/ai";
+
+const Setpassword = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const bg = useColorModeValue("#101010", "#F8F8F8");
+  const color = useColorModeValue("#101010", "#F8F8F8");
+  const btn = "#FE5454";
+  const sbtn = useColorModeValue("#F8F8F8", "#101010");
+  const cbox = useColorModeValue(
+    "rgba(64, 64, 64, 0.4)",
+    "rgba(210, 210, 210, 0.4)"
+  );
+  const field = useColorModeValue(
+    "rgba(210, 210, 210, 0.8)",
+    "rgba(64, 64, 64, 0.8)"
+  );
+  return (
+    <Signlayout>
+      <Center>
+        <Box w={"90%"}>
+          <Flex justifyContent="center" pt={10}>
+            <AiFillThunderbolt color={btn} fontSize={"80"} />
+          </Flex>
+          <Box mt={10}>
+            <Text
+              textAlign="center"
+              color={btn}
+              fontFamily={"Poppins"}
+              fontWeight="bold"
+              fontSize={"2xl"}
+            >
+              Set Password
+            </Text>
+          </Box>
+
+          <Box mt={16} pb={24}>
+            <Input
+              mb={5}
+              placeholder="Set Password"
+              border="none"
+              size="lg"
+              background={cbox}
+              fontFamily={"Poppins"}
+              fontWeight={"semi"}
+              fontSize={"md"}
+              textColor={sbtn}
+              _placeholder={{
+                fontFamily: "Poppins",
+                fontWeight: "semi",
+                fontSize: "md",
+                color: "rgba(64, 64, 64, 0.8)",
+              }}
+            />
+            <Input
+              mb={5}
+              placeholder="Re-type Password"
+              border="none"
+              size="lg"
+              background={cbox}
+              fontFamily={"Poppins"}
+              fontWeight={"semi"}
+              fontSize={"md"}
+              textColor={sbtn}
+              _placeholder={{
+                fontFamily: "Poppins",
+                fontWeight: "semi",
+                fontSize: "md",
+                color: "rgba(64, 64, 64, 0.8)",
+              }}
+            />
+            <Center>
+              <Button
+                w={"100%"}
+                size="lg"
+                fontSize={"md"}
+                borderRadius={5}
+                fontWeight="bold"
+                fontFamily={"Poppins"}
+                fontSize={"md"}
+                bg={btn}
+                color={color}
+                _hover={{ bg: "#f76565" }}
+                _active={{
+                  bg: { btn },
+                  transform: "scale(0.98)",
+                }}
+                _focus={{
+                  boxShadow:
+                    "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+                }}
+                type="submit"
+              >
+                Register
+              </Button>
+            </Center>
+            <Text
+              mt={3}
+              textAlign="center"
+              color={sbtn}
+              fontFamily={"Poppins"}
+              fontWeight="medium"
+              fontSize={"sm"}
+            >
+              By using this app, you agree to our<br></br> Terms of Use and
+              Privacy Policy
+            </Text>
+          </Box>
+        </Box>
+      </Center>
+    </Signlayout>
+  );
+};
+
+export default Setpassword;
---
+++ components/Signlayout.js
@@ -0,0 +1,29 @@
+import {
+  Button,
+  Heading,
+  useColorMode,
+  useColorModeValue,
+  Wrap,
+  IconButton,
+  Box,
+  Center,
+  Container,
+  Flex,
+} from "@chakra-ui/react";
+
+function Signlayout(props) {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const bg = useColorModeValue("#101010", "#F8F8F8");
+  const color = useColorModeValue("#F8F8F8", "#101010");
+  const btn = "#FE5454";
+  const sbtn = useColorModeValue("#F8F8F8", "#101010");
+  return (
+    <Box bg={bg} color={color} minHeight="100vh">
+      <Center>
+        <Box w={["90%", "90%", "70%", "80%"]}>{props.children}</Box>
+      </Center>
+    </Box>
+  );
+}
+export default Signlayout;
