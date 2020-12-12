8406458b9dd8becc8d700cfb6a0f87c255bc5273
MichaelDepp <michaeldepp7@gmail.com>
2020-12-12 21:54:30 +0800

    delete react-vocal

--- @untemps/react-vocal/src/index.js
+++
@@ -1,7 +0,0 @@
-import Vocal from './components/Vocal'
-import SpeechRecognitionWrapper from './core/SpeechRecognitionWrapper'
-
-export { default as useVocal } from './hooks/useVocal'
-export const isSupported = SpeechRecognitionWrapper.isSupported
-
-export default Vocal
